d57d9fda67629c4ff6d9b6c38b5701545c2bd839
Sindre Sorhus <sindresorhus@gmail.com>
2017-05-05 01:04:51 +0800

    Init

---
+++ test.js
@@ -0,0 +1,38 @@
+import path from 'path';
+import test from 'ava';
+import execa from 'execa';
+import cpy from 'cpy';
+import del from 'del';
+
+test.before(async () => {
+	await execa('npm', ['link']);
+});
+
+test.after(async () => {
+	await execa('npm', ['unlink']);
+});
+
+test('local', async t => {
+	await cpy(
+		['package.json', 'index.js', 'fixtures/cli.js'],
+		path.join(__dirname, 'fixtures/local/node_modules/import-local'),
+		{parents: true}
+	);
+
+	const {stdout} = await execa('import-local-fixture', {
+		preferLocal: false,
+		cwd: path.join(__dirname, 'fixtures/local')
+	});
+	t.is(stdout, 'local');
+
+	await del(path.join(__dirname, 'fixtures/local/node_modules'));
+});
+
+test('global', async t => {
+	const {stdout} = await execa('import-local-fixture', {
+		preferLocal: false,
+		cwd: path.join(__dirname, 'fixtures/global')
+	});
+	t.is(stdout, '');
+});
+
