7f85ab8273060f36b16ef09f352184b908bf960a
Zen-cronic <83657429+Zen-cronic@users.noreply.github.com>
2024-07-21 10:57:34 -0400

    Add TSDoc comments

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,17 @@
+/**
+Let a globally installed package use a locally installed version of itself if available
+
+@param __filename - The name of the package.
+
+@example
+```js
+import importLocal from 'import-local';
+
+if (importLocal(import.meta.url)) {
+	console.log('Using local version of this package');
+} else {
+	// Code for both global and local version here…
+}
+```
+*/
 export default function importLocal(__filename: string): boolean | undefined | unknown;
